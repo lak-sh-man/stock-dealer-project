39981ec446e19db26ebdf78418fc58f3ae4cb608
lak-sh-man <lakshman1122000@gmail.com>
2025-03-06 17:05:23 +0530

    commited

--- schema.py
+++ schema.py
@@ -3,9 +3,10 @@ from pydantic import BaseModel, Field
 
 class Register_pyd_schema(BaseModel):
     username: str = Field(..., min_length=3, max_length=50)
-    password: str = Field(..., min_length=2, max_length=255)
+    password: str = Field(..., min_length=3, max_length=255)
+    pass_confirm: str = Field(..., min_length=3, max_length=255)
 
 
 class Login_pyd_schema(BaseModel):
     username: str = Field(..., min_length=3, max_length=50)
-    password: str = Field(..., min_length=2, max_length=255)
+    password: str = Field(..., min_length=3, max_length=255)
